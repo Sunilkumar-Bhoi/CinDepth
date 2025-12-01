39bec8beef2768a9cf8b8e6bf398608739b51c1d
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-01 11:24:48 +0530

    escapeSequence

---
+++ elementOfC/programm01.c
@@ -0,0 +1,10 @@
+//Escape sequence or execution character
+
+#include<stdio.h>
+
+int main(){
+    printf("back space: move cursor to previous positionn of current line\b");
+    printf("\nnewlline : move cursor to beggining of next line");
+    printf("\ncarrieage return\rmove cursor to begging of current line");
+    return 0;
+}
