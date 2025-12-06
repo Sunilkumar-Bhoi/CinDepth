ddeddc9f6a53bafce295f8a608c872859e192298
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-06 19:25:52 +0530

    pyramid

--- controlStatment/pyramid/pyd27.c
+++ controlStatment/pyramid/pyd27.c
@@ -1,9 +1,9 @@
 /*
- 567898765
-  4567654
-   34543
-    232
-     1
+ 543212345
+  5432345
+   54345
+    545
+     5
 */
 
 #include<stdio.h>
@@ -13,13 +13,13 @@ int main(void){
         for(int j=1;j<=i;j++){
             printf(" ");
         }
-        int p=5+1-i;
+        int p=5;
         for(int k=1;k<=5-i+1;k++){
-            printf("%d",p++);
-        }
-        p=p-2;
-        for(int l=1;l<=5-i;l++){
             printf("%d",p--);
+        }
+        p=p+2;
+        for(int l=1;l<=5-i;l++){
+            printf("%d",p++);
         }
         printf("\n");
     }
---
+++ controlStatment/pyramid/pyd28.c
@@ -0,0 +1,39 @@
+/*
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+*/
+
+#include<stdio.h>
+
+int main(void){
+    int i,j,k,l,m,n;
+    printf("Enter number : ");
+    scanf("%d",&n);
+    for(i=1; i<=n; i++){
+        for(j=1;j<=n-i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=2*i-1;j++){
+            printf("*");
+        }
+        printf("\n");
+        }
+        n--;
+        for(i=1;i<=n;i++){
+        for(j=1;j<=i;j++){
+            printf(" ");
+        }
+        for(j=1;j<=2*(n-i)+1;j++){
+            printf("*");
+        }
+             printf("\n");
+    }
+    return 0;
+}
