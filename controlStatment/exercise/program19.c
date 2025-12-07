25933b855f01fc9027c6fa7280c5c26c213730b7
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-07 20:57:38 +0530

    doneexercise

--- controlStatment/exercise/program19.c
+++ controlStatment/exercise/program19.c
@@ -1,3 +1,24 @@
 #include<stdio.h>
 int main(void){
+	int x,y;
+	x=30,y=5,x;
+	if(x==10){
+		z++;
+	}
+	else if(x==20){
+		z---;
+	}
+	else if(x--30){
+		if(y==2){
+			z=z+3;
+		}
+		else if(y==4 && y==5){
+			z=z+10;
+		}
+		else	
+			z=z-2;
+	}
+	else
+		z=0;
 	return 0;
+}
