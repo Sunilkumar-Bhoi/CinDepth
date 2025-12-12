1e15ac1e03b1130f8f863e1d3d8cbbb02acc92bd
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-12 20:58:40 +0530

    dailyExercise

--- 05_controlStatment/exercise/program35.c
+++ 05_controlStatment/exercise/program35.c
@@ -1,3 +1,29 @@
+/*write the folowing loops without break statment*/
 #include<stdio.h>
 int main(void){
+	/*
+	for(i=1; ; i++){
+		if(i>5)
+			break;
+		printf("%d ",i);
+	}
+	*/
+	for(int i=1; i<=5; i++){
+		printf("%d ",i);
+	}
+	printf("\n");
+	/*
+	i=1,j=3
+	while(i<5){
+		if(j==0)
+			break;
+		printf("%d ",i++,j--)
+	}
+	*/
+	int i=1,j=3;
+	while(i<5 && j>0){
+		printf("%d ",i++,j--);/*warning and printf 1 2 3*/
+	}
+	printf("\n");
 	return 0;
+}
--- 05_controlStatment/exercise/program36.c
+++ 05_controlStatment/exercise/program36.c
@@ -1,3 +1,12 @@
 #include<stdio.h>
 int main(void){
+	int i=1,j=3;
+	while(i<5){
+		i++;
+		if(j==0)
+			break;/*i= 2 3 4 5, j=3, 2, 1*/
+		j--;
+	}
+	printf("%d %d\n",i,j);// 5,0
 	return 0;
+}
