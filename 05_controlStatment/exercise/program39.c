7e32642f9abb142e5cd47ca31e50326b4d84cf3c
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-13 14:44:33 +0530

    dailyEcercise

--- 05_controlStatment/exercise/program39.c
+++ 05_controlStatment/exercise/program39.c
@@ -1,5 +1,33 @@
+/*write this peice of code using while loop*/
 #include<stdio.h>
-int main(void){
-
+int main(void){	
+/*
+	i=1,z=0;
+	do{
+		if(i<5)
+		{
+			printf("%d ",i++);
+			z=z+2;
+		}
+	}while(i<5);
+*/
+	int i=1,z=0;
+	while(i<5){
+		printf("%d ",i);
+		i++;
+		z+=2;
+	}
+/*
+	do
+	{
+		printf("Enter employee ID(100-500): ");
+		scnaf("%d",&emp_id);
+	}while(emp_id<100 || emp_id>500)
+*/
+	int emp_id;
+	while(emp_id <100 || emp_id>500){
+		printf("Enter Employee id (100-500): ");
+		scanf("%d",&emp_id);
+	}
 	return 0;
 }
--- 05_controlStatment/exercise/program40.c
+++ 05_controlStatment/exercise/program40.c
@@ -1,19 +1,27 @@
-/*write this peice of code using while loop*/
+/*rewite this program using do while loop */
 #include<stdio.h>
-int main(void){	
-/*
-	i=1,z=0;
-	do(){
-		if(i<5)
-		{
-			printf("%d ",i++);
-			z=z+2;
-		}
-	}while(i<5);
-*/
-	int i=1,j=0;
-	while(i<5){
-		
+
+int main(void){
+	/*
+	int 1,b, h , sarea, vol;
+	printf ("Enter length, width, heighr of a cuniod: "):
+	scanf("%d%d%d",&l,&b,&h);
+	while(l<=c || b<=0 |; h<=0)
+	printf ("Enter length, width, heighr of a cuniod: "):
+	scanf("%d%d%d",&l,&b,&h);
 	}
+	sarea=2* (1*b + b*h + n*1);
+	vol=1*p*h;
+	printfi "Surface Area=zd, Volume=id\n", sarca, vol);
+	return 0;
+	*/
+	int l,b,h,sarea,vol;
+	do{
+		printf("Enter length, width, height of a cuboid :");/*we wont write two times because do while loop run for a single time even if condion is false*/
+		scanf("%d%d%d",&l,&b,&h);
+	} while (l<=0 || b<=0 || h<=0);
+	sarea=2*(l*b + b*h + h*l);
+	vol=l*b*h;
+	printf("Surface area=%d, Volume=%d\n",sarea,vol);
 	return 0;
 }
