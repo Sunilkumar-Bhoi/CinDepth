b698302f86d15990afe395d93cd862f8ea8c5b8d
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-14 17:40:03 +0530

    dailyExerxise

--- 05_controlStatment/exercise/program47.c
+++ 05_controlStatment/exercise/program47.c
@@ -1,3 +1,14 @@
+/*write a program to input 10 number and find the largest number */
 #include<stdio.h>
 int main(void){
+	int i,num,temp=0;
+	for(i=1; i<=10; i++)
+	{
+		printf("Enter number %d :",i);
+		scanf("%d",&num);
+		if(num>temp)
+			temp=num;
+	}
+	printf("Largest number is : %d\n",temp);
 	return 0;
+}
