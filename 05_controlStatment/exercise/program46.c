da00ad93e89d88e316e7ed2ce06a88de437010f5
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-14 17:33:33 +0530

    dailyExercise

--- 05_controlStatment/exercise/program46.c
+++ 05_controlStatment/exercise/program46.c
@@ -1,7 +1,15 @@
+/* Write a program to print numbers from 1 to 80 separeted by tab, 8 numbers per line.*/
 #include<stdio.h>
 int main(void){
-	int i=3;
-	if(i!=2 && i!=3)
-	printf("hi");
+	int i,j,k=1;
+	for(i=1; i<=10; i++)
+	{
+		for(j=1; j<=8; j++)
+		{
+			printf("%d\t",k);
+			k++;
+		}
+		printf("\n");
+	}
 	return 0;
 }
