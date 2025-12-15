8abd809bc6f796a42902e810fb3f128c166b8f0c
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-15 20:42:13 +0530

    dailyExercise

--- 05_controlStatment/exercise/program57.c
+++ 05_controlStatment/exercise/program57.c
@@ -1,3 +1,24 @@
+/*Write a program to enter a number and test whether it is a fibonacci number or not.*/
 #include<stdio.h>
 int main(void){
+	int num,a=0,b=1,next;
+	printf("Enter a number: ");
+	scanf("%d",&num);
+	if(num==0 || num==1)
+	{
+		printf("fibonacci number\n");
+		return 0;
+	}
+	while(b<num)
+	{
+		next=a+b;
+		a=b;
+		b=next;
+	}
+	if(b==num)
+		printf("Fibonacci number\n");
+	else
+		printf("not fibonacci number\n");
+
 	return 0;
+}
--- 05_controlStatment/exercise/program58.c
+++ 05_controlStatment/exercise/program58.c
@@ -1,3 +1,22 @@
+/*Find the sum of these series up t o n terms where x is an integer entered b y the user.
+(i) 1 + 11 + 111 + 1111+.....
+(ii)x+*+x+x+
+(tii) x-x°+x-x*+*/
 #include<stdio.h>
 int main(void){
+	int n;
+	printf("Enter a number : ");
+	scanf("%d",&n);
+	int a=1,j=10;
+	long long int sum=0;
+	for(int i=1;i<=n;i++)
+	{
+		printf("%d\n",a);
+		a=a+j;
+		sum+=a;
+		j=j*10;
+	}
+		printf("%lld\n",sum);
+
 	return 0;
+}
