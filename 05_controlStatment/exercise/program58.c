d9bd98b11f65131e9ffcedce609ce161e50d6e13
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-16 21:15:06 +0530

    dailyExercise

--- 05_controlStatment/exercise/program58.c
+++ 05_controlStatment/exercise/program58.c
@@ -1,22 +1,44 @@
 /*Find the sum of these series up t o n terms where x is an integer entered b y the user.
 (i) 1 + 11 + 111 + 1111+.....
-(ii)x+*+x+x+
-(tii) x-x°+x-x*+*/
+(ii)x+x^2+x^3+x^4+.....
+(tii) x-x^2+x^3-x^4+...*/
 #include<stdio.h>
 int main(void){
-	int n;
-	printf("Enter a number : ");
-	scanf("%d",&n);
-	int a=1,j=10;
+	int num,x;
+	printf("Enter a number of terms : ");
+	scanf("%d",&num);
+	printf("Enter the value of x : ");
+	scanf("%d",&x);
+	long int a=1,j=1;
 	long long int sum=0;
-	for(int i=1;i<=n;i++)
+	for(int i=1;i<=num;i++)
 	{
-		printf("%d\n",a);
-		a=a+j;
 		sum+=a;
 		j=j*10;
+		a+=j;
 	}
-		printf("%lld\n",sum);
+		printf("series 1+11+111+... : %lld\n",sum);
+	sum=0;
+	j=x;
+	for(int i=1; i<=num; i++)
+	{
+		sum+=x;
+		x=j*x;
+	}
+		printf("series x+x^2+x3... : %lld\n",sum);
 
+	sum=0;
+	x=j;
+	for(int i=1; i<=num; i++)
+	{
+		if(i%2==0)
+			sum-=x;
+		else 
+			sum+=x;
+
+		x=j*x;
+	}
+		printf("series x-x^2+x^3... : %lld\n",sum);
+	
 	return 0;
 }
