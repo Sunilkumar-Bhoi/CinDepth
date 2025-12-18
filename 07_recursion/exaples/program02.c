d514de42285ca92125d3fbbbcd2fd370f8591abd
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-18 20:16:38 +0530

    recursion

--- 07_recursion/exaples/program02.c
+++ 07_recursion/exaples/program02.c
@@ -1,1 +1,25 @@
-/*summation*/
+/*dcisplaying number from 1 to n*/
+#include<stdio.h>
+
+void display1(int num)
+{
+    if(num==0)
+        return;
+    printf("%d ",num);
+    display1(num-1);
+}
+void display2(int num)
+{
+    if(num==0)
+        return;
+    display2(num-1);
+    printf("%d ",num);
+}
+int main(void)
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d",&num);
+    display2(num);
+    return 0;
+}
---
+++ 07_recursion/exaples/program03.c
@@ -0,0 +1,20 @@
+/*pogram to display and find out sum of series*/
+#include<stdio.h>
+int display(int n)
+{ 
+    int sum;
+    if(n==0)
+        return 0;
+    sum = n + display(n-1);
+    printf("%d + ",n);
+    return sum;
+}
+int main(void)
+
+{
+    int num;
+    printf("enter number : ");
+    scanf("%d",&num);
+    printf("\b\b= %d\n",display(num));
+    return 0;
+}
