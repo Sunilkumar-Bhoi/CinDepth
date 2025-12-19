1132999fdcf00a9a90c62abe9dc68b8b0f52a3bd
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-19 23:13:34 +0530

    dailyExercise

--- 06_functions/exercise/program44.c
+++ 06_functions/exercise/program44.c
@@ -1,4 +1,4 @@
-/*write a function sumPdeivisor() that find the sum of proper divisior of number.Proper divisors of a number are
+/*write a function sumPdivisor() that find the sum of proper divisior of number.Proper divisors of a number are
 those numbers by which the number is divisible, except the number itself. For example proper divisors of 36 are 1, 2, 3, 4, 6,
 9 , 18.*/
 #include<stdio.h>
@@ -8,6 +8,16 @@ int main(void)
 	int num; 
 	printf("Enter a number: ");
 	scanf("%d",&num);
-	printf("sum of proper divisor of %d")
+	printf("sum of proper divisor of %d = %d",num,sumPdivisor(num));
 	return 0;
+}
+int sumPdivisor(int n)
+{
+	int sum=0;
+	for(int i=1; i<=n/2;i++)
+	{
+		if(n%i==0)
+			sum+=i;
+	}
+	return sum;
 }
--- 06_functions/exercise/program45.c
+++ 06_functions/exercise/program45.c
@@ -1,7 +1,31 @@
+/*a number is called perfect if the sum of proper divisors of that number is equal to the number.for example perfect number, since 28 1+2+4+7+14=28.write a program to print all the perfect numbers in agiven range*/
 #include<stdio.h>
-
+void PrintNumber(int a,int b);
+int sumPdivisor(int n);
 int main(void)
 {
-
+	int a,b;
+	printf("Enter range(small-large) :");
+	scanf("%d%d",&a,&b);
+	PrintNumber(a,b);
 	return 0;
+}
+void PrintNumber(int a, int b)
+{
+	for(int i=a; i<=b; i++)
+	{
+		if(sumPdivisor(i)==i)
+			printf("%d ",i);
+	}
+}
+int sumPdivisor(int n){
+	int sum=0;
+	if(n==1)
+		return 0;
+	for(int i=1; i<=n/2; i++)
+	{
+		if(n%i==0)
+			sum+=i;
+	}
+	return sum;
 }
