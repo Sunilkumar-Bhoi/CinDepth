e9b4aac429729f913203bb0383cf856c65c8c1a7
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-20 20:27:42 +0530

    dailyExercise

--- 06_functions/exercise/program46.c
+++ 06_functions/exercise/program46.c
@@ -1,7 +1,36 @@
+/*amicable number: if the sum of perfect divisor is equal of two number
+write a function that print pair of amicable number in range*/
 #include<stdio.h>
-
+void printDivisor(int x,int y);
+int sumPdivisor(int n);
 int main(void)
 {
-
+	int a,b;
+	printf("Enter range(min-max): ");
+	scanf("%d%d",&a,&b);
+	printDivisor(a,b);
 	return 0;
+}
+void printDivisor(int x, int y)
+{
+	for(int i=x; i<=y; i++)
+	{
+			int a=sumPdivisor(i);
+			if(a>i && a<=y)
+			{
+				int b=sumPdivisor(a);
+				if(b==i)
+					printf("%d %d\t",i,a);
+			}
+	}
+}
+int sumPdivisor(int n)
+{
+	int sum=0;
+	for(int i=1; i<=n/2; i++)
+	{
+		if(n%i==0)
+			sum+=i;
+	}
+	return sum;
 }
