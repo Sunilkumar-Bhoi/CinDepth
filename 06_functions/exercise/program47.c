9c416c6c7cb857128cc310c02239c184acd3d433
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-20 20:50:37 +0530

    dailyExercise

--- 06_functions/exercise/program47.c
+++ 06_functions/exercise/program47.c
@@ -1,7 +1,39 @@
+/*write a single function to print both amicable pair and perfect number*/
 #include<stdio.h>
-
+void func(int a,int b)
+{
+	
+	for(int i=a; i<=b; i++)
+	{
+		int sum1=0;
+		for(int j=1; j<=i/2; j++){
+			if(i%j==0)
+			{
+				sum1+=j;
+			}	
+		}
+		if(sum1==i)
+		{	
+			printf("perfect number: %d\n",i);
+		}
+		int sum2=0;
+		if(sum1>i && sum1<=b)
+		{
+			for(int k=1;k<=sum1/2; k++)
+			{
+				if(sum1%k==0)
+					sum2+=k;
+			}
+		}
+		if(i==sum2)
+			printf("amicable pair: %d %d",i,sum1);
+	}
+}
 int main(void)
 {
-
+	int a,b;
+	printf("Enter range(min-max): ");
+	scanf("%d%d",&a,&b);
+	func(a,b);
 	return 0;
 }
