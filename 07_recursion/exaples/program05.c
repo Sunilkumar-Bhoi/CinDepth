2848c5932b425387bf45d56f8840df018fbcfd11
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-22 22:53:11 +0530

    dailyExercise

--- 07_recursion/exaples/program05.c
+++ 07_recursion/exaples/program05.c
@@ -1,25 +1,24 @@
 /*P7.5 Progran to convert a positive decimal number to Binary, Octal or Hexadecimal*/
-
 #include<stdio.h>
-int octal(int n);
-int hexa(int n);
+void convert(int n, int b);
 int main(void)
 {
-    char base;
-    int deci;
-    printf("Enter a base 'o' or 'h': ");
-    scanf(" %c",&base);
-    printf("Enter decimal number: ");
-    scanf("%d",&deci);
-    if(base=='o')
-        printf("%d decimal = %o octal",deci,octal(deci));
-    else   
-        printf("%d in decimal = %x in hexadecimal",deci,hexa(deci));
+    int num;
+    printf("Enter a decimal number: ");
+    scanf("%d",&num);
+    convert(num,2);     printf("\n");
+    convert(num,8);     printf("\n"); 
+    convert(num,16);   printf("\n");
     return 0;
 }
-int octal(int n)
+void convert(int n,int b)
 {
-    if(n/10<8)
-        return n;
-    return n%10+1
+    int rem=n%b;
+    if(n==0)
+        return;
+  convert(n/b,b);
+    if(rem<10)
+      printf("%d",rem);
+    else  
+        printf("%c",rem-10+'A');
 }
