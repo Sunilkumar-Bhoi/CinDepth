3f141bb676d3bc89ab5841d450fee312df3c67e8
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-29 19:39:49 +0530

    callbyreferance

---
+++ 09_pointers/examples/program17.c
@@ -0,0 +1,17 @@
+/*call by ref*/
+#include<stdio.h>
+void ref(int *p, int *q);
+int main(void)
+{
+    int a=5,b=6;
+    printf("a=%d, b=%d\n",a,b);
+    ref(&a,&b);
+    printf("a=%d, b=%d\n",a,b);
+    return 0;
+}
+void ref(int *p, int *q)
+{
+    (*p)++;
+    (*q)++;
+    printf("*p=%d, *q=%d\n",(*p),*q);
+}
