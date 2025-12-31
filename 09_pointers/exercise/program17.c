7cac07d961326ac89d164d11329d893268943e75
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-31 21:50:19 +0530

    dailyExercise

--- 09_pointers/exercise/program17.c
+++ 09_pointers/exercise/program17.c
@@ -2,6 +2,8 @@
 
 int main(void)
 {
-
+    int arr[6] = {1,2,3,4,5,6};
+    int *p=arr;
+    printf("Size of p=%lu, Size of arr=%lu\n",sizeof(p),sizeof(arr));
     return 0;
 }
--- 09_pointers/exercise/program19.c
+++ 09_pointers/exercise/program19.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
-
+int a=5, b=10;
+void change(int *p)
+{
+    p=&a;
+}
+void change2(int **pp)
+{
+    *pp=&b;
+}
 int main(void)
 {
-
+    int x=20, *ptr=&x;
+    printf("%d ",*ptr);//20
+    change(ptr);
+    printf("%d ",*ptr);//5
+    change2(&ptr);
+    printf("%d ",*ptr);
     return 0;
 }
