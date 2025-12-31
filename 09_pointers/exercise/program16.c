888d0f11e18c802ca68e469b89c17f608e15864d
Sunilkumar-Bhoi <bhois0648@gmail.com>
2025-12-31 21:22:23 +0530

    dailyExercise

--- 09_pointers/exercise/program16.c
+++ 09_pointers/exercise/program16.c
@@ -4,8 +4,8 @@ int main(void)
 {
     int x, arr[8] ={11,22,33,44,55,66,77,88};
     int *p,*q;
-    q=arr/2;
-    p=q*2;
+    q=arr/2; //division is not an pointer arithematic
+    p=q*2;//multply is not an pointer arithematic 
     printf("%d  %d\n",*p,*q);
     return 0;
 }
